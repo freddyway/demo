0463de9d6d83f3874ffcaff356f95033a68884ff
alfre <alf19640408@gmail.com>
2022-10-05 20:15:16 +0200

    mas cambio

--- src/main/java/com/example/demo/repository/UsuarioRepository.java
+++ src/main/java/com/example/demo/repository/UsuarioRepository.java
@@ -3,5 +3,6 @@ package com.example.demo.repository;
 import com.example.demo.model.Usuario;
 import org.springframework.data.repository.CrudRepository;
 
+//Mas cambios
 public interface UsuarioRepository extends CrudRepository<Usuario,Long> {
 }
