3fb4db76879eec53b66126c5b150d13a240e1dde
alfre <alf19640408@gmail.com>
2022-10-05 20:09:04 +0200

    Initial commit

---
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -0,0 +1,33 @@
+package com.example.demo;
+
+import com.example.demo.model.Usuario;
+import com.example.demo.repository.UsuarioRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+public class DemoApplication {
+	private static final Logger log = LoggerFactory.getLogger(DemoApplication.class);
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+	}
+
+	@Bean
+	public CommandLineRunner demo(UsuarioRepository repository) {
+		return (args) -> {
+			// save a few customers
+			repository.save(new Usuario("alfredo","alfredo@gmail.com"));
+			repository.save(new Usuario("ana maria","anamaria@gmail.com"));
+			repository.save(new Usuario("pablo","pablo@gmail.com"));
+
+			for (Usuario customer : repository.findAll()) {
+				log.info(customer.toString());
+			}
+		};
+	}
+}
---
+++ src/main/java/com/example/demo/model/Usuario.java
@@ -0,0 +1,49 @@
+package com.example.demo.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import java.io.Serializable;
+
+@Entity
+public class Usuario implements Serializable {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    private String email;
+
+    protected Usuario() {  }
+
+    public Usuario(String name, String email) {
+        this.name = name;
+        this.email = email;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    @Override
+    public String toString() {
+        return "Usuario{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", email='" + email + '\'' +
+                '}';
+    }
+}
